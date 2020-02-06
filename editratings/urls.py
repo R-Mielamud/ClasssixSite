6bbc37e183f3db608edb2ddf70698842a6787830
R-Mielamud <2m.roman2@gmail.com>
2020-02-06 12:01:49 +0200

    inital commit

--- editratings/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from editratings.views import EditratingsView
-
-urlpatterns = [
-    url(r"^editratings/$", EditratingsView.as_view())
-]
--- main/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from main.views import RegistrationAndMainView
-
-urlpatterns = [
-    url(r"^$", RegistrationAndMainView.as_view())
-]
