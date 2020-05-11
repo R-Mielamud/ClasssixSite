d1c9e284a1acc77a4f5bd86efe41c033ef9e39e0
R-Mielamud <2m.roman2@gmail.com>
2020-05-11 16:23:39 +0300

    add postgresql

--- ClasssixSite/settings.py
+++ ClasssixSite/settings.py
@@ -98,12 +98,24 @@ WSGI_APPLICATION = 'ClasssixSite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if not os.environ.get("POSTGRES_PASSWD"):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': "class6_db",
+            'USER': 'cls6_db',
+            'PASSWORD': os.environ.get("POSTGRES_PASSWD"),
+            'HOST': 'localhost',
+            'PORT': ''
+        }
+    }
 
 
 # Password validation
